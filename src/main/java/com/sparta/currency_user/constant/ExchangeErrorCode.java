5e4994ea5a05bc7f95bc1641c926d5628203b464
이자드 <kimminkyeong0418@gmail.com>
2024-11-26 23:08:06 +0900

    feat: 환전 기록 조회 에러 처리 추가

--- src/main/java/com/sparta/currency_user/constant/ExchangeErrorCode.java
+++ src/main/java/com/sparta/currency_user/constant/ExchangeErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ExchangeErrorCode implements ErrorCode {
     INV(HttpStatus.BAD_REQUEST, "잘못된 요청"),
-    NOT_FOUND(HttpStatus.NOT_FOUND,"")
+    NOT_FOUND(HttpStatus.NOT_FOUND,"환전 기록을 찾을 수 없습니다")
     ;
     private final HttpStatus httpStatus;
     private final String message;
