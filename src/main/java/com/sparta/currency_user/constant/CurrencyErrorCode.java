e253b05cddc1deacafa3b065dd194cbf95f755a3
이자드 <kimminkyeong0418@gmail.com>
2024-11-26 22:57:57 +0900

    feat: 환전 생성 에러 처리 추가

--- src/main/java/com/sparta/currency_user/constant/CurrencyErrorCode.java
+++ src/main/java/com/sparta/currency_user/constant/CurrencyErrorCode.java
@@ -4,11 +4,12 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-@Getter
 @RequiredArgsConstructor
-public enum ExchangeErrorCode implements ErrorCode {
-    INV(HttpStatus.BAD_REQUEST, "잘못된 요청"),
-    NOT_FOUND(HttpStatus.NOT_FOUND,"")
+@Getter
+public enum CurrencyErrorCode implements ErrorCode{
+    NOT_FOUND(HttpStatus.NOT_FOUND, "환전 대상 통화를 찾을 수 없습니다"),
+
+
     ;
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/com/sparta/currency_user/constant/UserErrorCode.java
+++ src/main/java/com/sparta/currency_user/constant/UserErrorCode.java
@@ -3,12 +3,12 @@ package com.sparta.currency_user.constant;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-
-@Getter
 @RequiredArgsConstructor
-public enum ExchangeErrorCode implements ErrorCode {
-    INV(HttpStatus.BAD_REQUEST, "잘못된 요청"),
-    NOT_FOUND(HttpStatus.NOT_FOUND,"")
+@Getter
+public enum UserErrorCode implements ErrorCode{
+    NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다"),
+
+
     ;
     private final HttpStatus httpStatus;
     private final String message;
