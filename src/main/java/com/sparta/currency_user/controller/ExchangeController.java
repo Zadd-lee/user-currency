18a5147d1e337831ba7fe65e7452f8207f5c6fdc
이자드 <kimminkyeong0418@gmail.com>
2024-11-26 23:54:27 +0900

    feat: 환전- 사용자 삭제 기능 추가

--- src/main/java/com/sparta/currency_user/controller/ExchangeController.java
+++ src/main/java/com/sparta/currency_user/controller/ExchangeController.java
@@ -3,6 +3,7 @@ package com.sparta.currency_user.controller;
 import com.sparta.currency_user.dto.CreateExchangeRequestDto;
 import com.sparta.currency_user.dto.ExchangeResponseDto;
 import com.sparta.currency_user.service.ExchangeService;
+import com.sparta.currency_user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,23 +17,32 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ExchangeController {
 
-    private final ExchangeService service;
+    private final ExchangeService exchangeService;
+    private final UserService userService;
+
+
     @PostMapping
     public ResponseEntity<ExchangeResponseDto> create(@Valid @RequestBody CreateExchangeRequestDto dto) {
-        ExchangeResponseDto responseDto = service.create(dto);
+        ExchangeResponseDto responseDto = exchangeService.create(dto);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
     @GetMapping("/{userId}")
     public ResponseEntity<List<ExchangeResponseDto>> getAll(@PathVariable Long userId) {
-        List<ExchangeResponseDto> responseDtos = service.findByUserId(userId);
+        List<ExchangeResponseDto> responseDtos = exchangeService.findByUserId(userId);
         return new ResponseEntity<>(responseDtos, HttpStatus.OK);
     }
 
     @PatchMapping("/{exchangeId}")
     public ResponseEntity<ExchangeResponseDto> cancel(@PathVariable Long exchangeId) {
-        ExchangeResponseDto responseDto = service.cancel(exchangeId);
+        ExchangeResponseDto responseDto = exchangeService.cancel(exchangeId);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<ExchangeResponseDto> delete(@PathVariable Long userId) {
+        userService.deleteUserById(userId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
