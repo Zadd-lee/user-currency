c6c7b9e5c8c6b34a5fe11f03c4add3b036290f28
이자드 <kimminkyeong0418@gmail.com>
2024-11-26 15:18:16 +0900

    feat: time entity 생성 및 적용

---
+++ src/main/java/com/sparta/currency_user/entity/TimeEntity.java
@@ -0,0 +1,22 @@
+package com.sparta.currency_user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.sql.Timestamp;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class TimeEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    Timestamp createdAt;
+    @LastModifiedDate
+    Timestamp modifiedAt;
+}
