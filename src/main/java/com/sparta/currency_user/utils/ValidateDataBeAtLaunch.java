9773d0fab41147edd7d662eed10f4dcadf08a78a
이자드 <kimminkyeong0418@gmail.com>
2024-11-27 22:36:11 +0900

    feat: Lv 4 스프링 실행시 검증 처리 구현 완료

---
+++ src/main/java/com/sparta/currency_user/utils/ValidateDataBeAtLaunch.java
@@ -0,0 +1,27 @@
+package com.sparta.currency_user.utils;
+
+import com.sparta.currency_user.entity.Currency;
+import com.sparta.currency_user.repository.CurrencyRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class ValidateDataBeAtLaunch {
+    private final CurrencyRepository currencyRepository;
+
+    @PostConstruct
+    public void validateCurrency() {
+        List<Currency> all = currencyRepository.findAll();
+        for (Currency currency : all) {
+            if (currency.getExchangeRate().signum() == -1) {
+                log.warn("검증 오류 - {}/{} : 음수 값", currency.getClass().getSimpleName(), currency.getCurrencyName());
+            }
+        }
+    }
+}
